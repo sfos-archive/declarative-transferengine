47cc0a4f4cab45a95169c2c468c8bee41abadb46
Aaron McCarthy <aaron.mccarthy@jollamobile.com>
2015-03-10 14:26:57 +1000

    [declarative-transferengine] Use ContentAction from nemo-qml-plugin-contentaction. Contributes to JB#9477
    ContentAction has been moved from the transfer engine plugin to a new
    QML plugin provided by libcontentaction. Remove ContentAction and use
    the one from libcontentaction instead.

--- declarative/declarative_plugin.h
+++ declarative/declarative_plugin.h
@@ -13,39 +13,6 @@ public:
    virtual ~AppTranslator();
 };
 
-class DeclarativeContentAction: public QObject
-{
-    Q_OBJECT
-    Q_PROPERTY(Error error READ error NOTIFY errorChanged)
-    Q_ENUMS(Error)
-
-public:
-    enum Error {
-        NoError,
-        FileTypeNotSupported,
-        FileDoesNotExist,
-        InvalidUrl
-    };
-
-    explicit DeclarativeContentAction(QObject *parent = 0);
-    Q_INVOKABLE bool trigger(const QUrl &url);
-
-    Error error() const;
-
-signals:
-    void errorChanged();
-private:
-
-    Error m_error;
-};
-
-static QObject *content_action(QQmlEngine *engine, QJSEngine *scriptEngine)
-{
-    Q_UNUSED(engine)
-    Q_UNUSED(scriptEngine)
-    return new DeclarativeContentAction;
-}
-
 class DeclarativePlugin : public QQmlExtensionPlugin
 {
     Q_OBJECT
